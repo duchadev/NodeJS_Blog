0962a213ad05b429c6bf035150695b8e901744bf
duchadev <95076379+hannguu@users.noreply.github.com>
2024-05-18 22:47:36 +0700

    Create: routes, controllers

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const newsRouter = require('../routes/news')
+const siteRouter = require('../routes/site')
+function route(app)
+{
+    app.use('/news', newsRouter);
+    // app.use('/news/hoc-lap-trinh', newsRouter);
+    // app.use('/news/hoc-lap-trinh', newsRouter);
+
+    app.use('/', siteRouter);
+
+
+}
+
+module.exports = route;
---
+++ src/routes/news.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const router = express.Router();
+const newsController = require('../app/controllers/NewsController');
+
+router.use('/hoc-lap-trinh', newsController.show)
+router.use('/hoc-nodejs', newsController.show1)
+
+router.use('/', newsController.index);
+
+module.exports = router;
