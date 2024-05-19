355236c0837e3a967d5cb910aacd672e2308d71d
duchadev <95076379+hannguu@users.noreply.github.com>
2024-05-19 15:19:52 +0700

    install prettier

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const newsRouter = require('../routes/news');
 const siteRouter = require('../routes/site');
 function route(app) {
-    app.use('/news', newsRouter);
+                            app.use('/news', newsRouter);
     // app.use('/news/hoc-lap-trinh', newsRouter);
     // app.use('/news/hoc-lap-trinh', newsRouter);
 
